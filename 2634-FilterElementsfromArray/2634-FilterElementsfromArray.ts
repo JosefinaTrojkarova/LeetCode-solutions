79ce8038c0ecf893d7ad1e1059b306c431727425
Andrej Pečeňa <159439229+JosefinaTrojkarova@users.noreply.github.com>
2025-04-28 22:17:47 +0200

    File updated at 4/28/2025, 10:17:47 PM

--- 2634-FilterElementsfromArray/2634-FilterElementsfromArray.ts
+++ 2634-FilterElementsfromArray/2634-FilterElementsfromArray.ts
@@ -1,12 +1,8 @@
-// Last updated: 4/28/2025, 10:17:22 PM
-type Fn = (n: number, i: number) => any
-
-function filter(arr: number[], fn: Fn): number[] {
-    let newArr: number[] = []
-    for (let i = 0; i < arr.length; i++) {
-        if (fn(arr[i], i)) {
-            newArr.push(arr[i])
-        }
+// Last updated: 4/28/2025, 10:17:47 PM
+function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+    let newArr = arr;
+    for (let i = 0; arr.length > i; i++) {
+        newArr[i] = fn(arr[i], i);
     }
-    return newArr
+    return newArr;
 };
