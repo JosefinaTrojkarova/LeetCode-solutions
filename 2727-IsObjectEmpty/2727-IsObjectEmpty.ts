60a7cd0e4b39aff00cbf6aa1e2ac01d5a1df0c49
Andrej Pečeňa <159439229+JosefinaTrojkarova@users.noreply.github.com>
2025-04-30 18:19:26 +0200

    File updated at 4/30/2025, 6:19:26 PM

--- 2727-IsObjectEmpty/2727-IsObjectEmpty.ts
+++ 2727-IsObjectEmpty/2727-IsObjectEmpty.ts
@@ -1,7 +1,11 @@
-// Last updated: 4/30/2025, 6:15:34 PM
+// Last updated: 4/30/2025, 6:19:26 PM
 type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
 type Obj = Record<string, JSONValue> | JSONValue[]
 
 function isEmpty(obj: Obj): boolean {
-    return JSON.stringify(obj).length === 2
+    if (Array.isArray(obj)) {
+        return obj.length === 0
+    } else {
+        return Object.keys(obj).length === 0
+    }
 };
