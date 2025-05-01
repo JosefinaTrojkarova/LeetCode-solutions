918a88c42b96465e13700d4018daf92fca1f12b4
Andrej Pečeňa <159439229+JosefinaTrojkarova@users.noreply.github.com>
2025-05-01 20:27:13 +0200

    File updated at 5/1/2025, 8:27:13 PM

--- 2625-FlattenDeeplyNestedArray/2625-FlattenDeeplyNestedArray.ts
+++ 2625-FlattenDeeplyNestedArray/2625-FlattenDeeplyNestedArray.ts
@@ -1,12 +1,11 @@
-// Last updated: 5/1/2025, 8:26:13 PM
+// Last updated: 5/1/2025, 8:27:13 PM
 type MultiDimensionalArray = (number | MultiDimensionalArray)[];
 
 var flat = function (arr:  MultiDimensionalArray, n: number, res = []):  MultiDimensionalArray {
     for(let i = 0; i < arr.length; i++) {
-        if(Array.isArray(arr[i]) && n)
+        if (Array.isArray(arr[i]) && n) {
             flat(arr[i] as MultiDimensionalArray, n - 1, res);
-        else
-            res.push(arr[i]);
+        } else res.push(arr[i]);
     }
 
     return res;
